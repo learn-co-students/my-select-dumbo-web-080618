3b5f719b9bae4312214acbe17fed90475ff3c832
Felix Chan <felixchan29@gmail.com>
2018-07-21 19:21:05 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,16 @@
 def my_select(collection)
- # your code here!
+  # your code here!
+  new_collection = []
+  i = 0
+  while i < collection.length do
+    if yield(collection[i])
+      new_collection << collection[i]
+    end
+    i += 1
+  end
+  new_collection
 end
+  
+#tests
+#puts (1..10).select{ |i|  i % 3 == 0 }
+#puts my_select([1,2,3,4,5,6,7,8,9,10]) { |i|  i % 3 == 0 }
