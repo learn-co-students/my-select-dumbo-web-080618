c8d44557e1d3635c1b72589370c81f571c91d540
Juan Elias Castillo <eliasiscoding@gmail.com>
2018-07-26 15:18:00 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
 def my_select(collection)
- # your code here!
+ i = 0 
+ newarray = []
+ while i < collection.length 
+    if yield(collection[i])
+      true 
+      newarray.push(collection[i])
+      end
+    i += 1
+  end
+  newarray 
 end
